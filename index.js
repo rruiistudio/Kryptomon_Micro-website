c19497d2ec48ea967dd85415135b10f45cf79baf
unknown <info@rruii.io>
2021-11-20 22:05:13 +0800

    test

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //UTILITY FUNCTIONS
 //import _ from 'lodash';
-import {loadconfirm, redirect} from '../scripts/utilities.js'
-import mobileAndTabletCheck from '../scripts/utilities.js'
+import {loadconfirm, redirect} from './scripts/utilities.js'
+import mobileAndTabletCheck from './scripts/utilities.js'
 var port = process.env.port || 5500;
 
 
 mobileAndTabletCheck();
 redirect();
 loadconfirm();
-app.listn(port);
+app.listen(port);
 
  
 
