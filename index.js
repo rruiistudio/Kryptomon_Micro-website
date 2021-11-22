99adf25f6cfcf9aaa7b1c163b034c3b85e53f48c
unknown <info@rruii.io>
2021-11-22 16:32:12 +0800

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //UTILITY FUNCTIONS
-
-//import {loadconfirm, redirect} from '.utilities.js'
-//import mobileAndTabletCheck from 'utilities.js'
+//import _ from 'lodash';
+import {loadconfirm, redirect} from '../scripts/utilities.js'
+import mobileAndTabletCheck from '../scripts/utilities.js'
+var port = process.env.port || 5500;
 
 
 mobileAndTabletCheck();
 redirect();
 loadconfirm();
-
+app.listen(port);
 
  
 
